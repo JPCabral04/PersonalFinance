1c1eaa3493e5e7ebd31a18ea1306fc795867688a
JPCabral04 <jpqcabral04@gmail.com>
2025-05-23 16:52:05 -0300

    feat: initialize Personal Finance API with TypeORM and Express

---
+++ PersonalFinanceAPI/src/index.ts
@@ -0,0 +1,19 @@
+import { AppDataSource } from './data-source';
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando em: http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Erro ao conectar no banco:', err);
+  });
