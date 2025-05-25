e9d5993df1a56c0daad92dfba3b5266eae69e87c
JPCabral04 <jpqcabral04@gmail.com>
2025-05-25 20:04:25 -0300

    feat: configure database for testing environment and add Jest configuration

--- PersonalFinanceAPI/src/data-source.ts
+++ PersonalFinanceAPI/src/data-source.ts
@@ -4,10 +4,14 @@ import { Account } from './entities/Account';
 import { Transaction } from './entities/Transaction';
 import 'dotenv/config';
 
+const isTest = process.env.NODE_ENV === 'test';
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  database: isTest ? 'database_test' : process.env.DATABASE_NAME,
   synchronize: true,
   logging: false,
+  dropSchema: isTest,
   entities: [User, Account, Transaction],
 });
