b968496359ee964fcc12d2fc6c54ea61d198dbf4
JPCabral04 <jpqcabral04@gmail.com>
2025-05-27 12:51:43 -0300

    feat: add CORS support and restructure server initialization

--- PersonalFinanceAPI/src/app.ts
+++ PersonalFinanceAPI/src/app.ts
@@ -1,36 +1,20 @@
-import { AppDataSource } from './data-source';
 import express from 'express';
-import dotenv from 'dotenv';
 import { errorHandler } from './middlewares/errorHandler';
+import cors from 'cors';
 import authRoute from './routes/authRoute';
 import userRoute from './routes/userRoute';
 import accountRoute from './routes/accountRoute';
 import transactionRoute from './routes/transactionRoute';
 import { notFoundHandler } from './middlewares/notFoundHandler';
 
-dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 3000;
-
+export const app = express();
+app.use(cors());
 app.use(express.json());
 
-// Rotas
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
 app.use('/api/account', accountRoute);
 app.use('/api/transaction', transactionRoute);
 
-// Middleware para lidar com rotas não encontradas
 app.use(notFoundHandler);
-// Middleware para lidar com erros
 app.use(errorHandler);
-
-AppDataSource.initialize()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando em: http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error('Erro ao conectar no banco:', err);
-  });
