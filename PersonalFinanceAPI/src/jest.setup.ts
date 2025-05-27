c7bb35ca158d923b4248fee025d5c4296da88129
JPCabral04 <jpqcabral04@gmail.com>
2025-05-27 12:56:29 -0300

    feat: update Jest setup for database initialization and cleanup

--- PersonalFinanceAPI/src/jest.setup.ts
+++ PersonalFinanceAPI/src/jest.setup.ts
@@ -1,4 +1,22 @@
 import 'reflect-metadata';
 import dotenv from 'dotenv';
+import { AppDataSource } from './data-source';
 
 dotenv.config({ path: '.env.test.local' });
+
+beforeAll(async () => {
+  await AppDataSource.initialize();
+  await AppDataSource.synchronize();
+});
+
+afterAll(async () => {
+  await AppDataSource.destroy();
+});
+
+beforeEach(async () => {
+  const entities = AppDataSource.entityMetadatas;
+  for (const entity of entities) {
+    const repository = AppDataSource.getRepository(entity.name);
+    await repository.clear();
+  }
+});
